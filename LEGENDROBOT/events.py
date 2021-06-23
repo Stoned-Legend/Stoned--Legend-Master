10b02ec6c06b2ba0147aa11960190c7a1250c1ec
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-23 13:53:47 +0000

    Update events.py

--- LEGENDROBOT/events.py
+++ LEGENDROBOT/events.py
@@ -10,12 +10,6 @@ from telethon import events
 from pymongo import MongoClient
 from LEGENDROBOT import MONGO_DB_URI
 from LEGENDROBOT import telethn
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["darkuserbot"]
-gbanned = db.gban
-
 def register(**args):
     """ Registers a new message. """
     pattern = args.get("pattern", None)
