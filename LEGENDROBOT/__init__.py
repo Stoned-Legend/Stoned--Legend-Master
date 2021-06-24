7aab7604fd998b5902ae74b9583dc82f261104f5
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-24 06:58:43 -0400

    Update __init__.py

--- LEGENDROBOT/__init__.py
+++ LEGENDROBOT/__init__.py
@@ -102,7 +102,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from SuzieRoBot.config import Development as Config
+    from LEGENDROBOT.config import Development as Config
 
     TOKEN = Config.TOKEN
 
